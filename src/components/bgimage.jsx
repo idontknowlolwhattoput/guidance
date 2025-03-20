123d75829403e57e71befdecd8b9681b3079fe11
idontknowlolwhattoput <132071917+idontknowlolwhattoput@users.noreply.github.com>
2025-03-20 22:13:33 +0800

    Third Commit

--- src/components/bgimage.jsx
+++ src/components/bgimage.jsx
@@ -2,18 +2,18 @@ import background from "../assets/bgimage.webp"
 
 function BgImage() {
     return (
-        <div class="relative w-full h-[500px]">
-        <img src={background} alt="Background" class="w-full h-full object-cover brightness-35"/>
-        <div class="absolute inset-0 flex flex-col justify-center items-center text-white">
-            <h1 class="pb-7 text-7xl font-bold">University of Pasig</h1>
-            <p class="text-white text-lg font-semibold text-center leading-tight max-w-3xl">
+        <div className="relative w-full h-[500px]">
+        <img src={background} alt="Background" className="w-full h-full object-cover brightness-35"/>
+        <div className="absolute inset-0 flex flex-col justify-center items-center text-white">
+            <h1 className="bebas tracking-wider text-9xl font-bold">University of Pasig</h1>
+            <p className="open-sans text-white text-lg font-semibold text-center leading-tight max-w-3xl">
             Pamantasan ng Lungsod ng Pasig is a top institution committed to academic excellence, ethical leadership, and community engagement. It equips students with skills and knowledge to thrive in both global and local industries, fostering responsible and competent graduates who drive positive change in society.
             </p>
             <div className="grid grid-cols-2 gap-5 pt-5">
             <button className="bg-blue-600 text-white px-4 py-2 rounded-lg shadow-md font-bold hover:bg-blue-700 w-40">
               Learn more
             </button>
-            <button class="border-2 border-blue-400 text-blue-400 px-4 py-2 rounded-lg hover:bg-blue-400 hover:text-white transition">
+            <button className="border-2 border-blue-400 text-blue-400 px-4 py-2 rounded-lg hover:bg-blue-400 hover:text-white transition">
                Login
             </button>
             </div>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,23 +1,26 @@
 import logo from "../assets/plplogo.svg";
+import { CgMoreR } from "react-icons/cg";
+import more from "../assets/more.svg"
 
 function Navbar() {
     return (
         <nav className="flex items-center justify-between  px-6 py-4">
         <div className="flex items-center space-x-3"> 
             <img src={logo} alt="plp logo" className="w-15 h-15" />
-        
-            <span className="text-2xl font-bold text-blue-900">University of Pasig</span>
+            <span className="hidden md:flex font-poppins text-2xl font-bold text-blue-900"> University of Pasig </span>
         </div>
     
-        <div class="flex gap-3 items-center space-x-12">
+        <div class="hidden md:flex poppins  items-center space-x-12">
           <a href="#" className="text-blue-900 hover:text-blue-600 font-bold">Home</a>
           <a href="#" className="text-blue-900 hover:text-blue-600 font-bold">About</a>
           <a href="#" className="text-blue-900 hover:text-blue-600 font-bold">Transactions</a>
           <button className="bg-blue-600 text-white px-4 py-2 rounded-lg shadow-md font-bold hover:bg-blue-700 w-40">
             Login
           </button>
-</div>
-
+        </div>
+        <div className="inline md:hidden mr-[2vw]">
+          <img src={more} alt="more" className="more w-10 h-10" />
+        </div>
     </nav>
     
     );
